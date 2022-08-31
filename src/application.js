5f50526e7ac2aa76c600ee4d18e146cc273c408c
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2022-08-31 19:49:26 +1200

    added basic drawing of particles.

--- src/application.js
+++ src/application.js
@@ -2,6 +2,18 @@ import { Particle } from "./particles/particle";
 
 export class Application {
     constructor(width = 400, height = 400) {
-        
+        this.width = width;
+        this.height = height;
+        this.current_grid = [];
+        for (let y = 0; y < height; y++) {
+            this.current_grid[y] = [];
+            for (let x = 0; x < width; x++) {
+                this.current_grid[y][x] = null;
+            }
+        }
+        this.organisms = [];
+        this.light_level = 100;
+        this.oxygen_level = 100;
+        this.temperature_level = 100;
     }
 }
--- src/main.js
+++ src/main.js
@@ -3,15 +3,32 @@ import p5 from "p5";
 import { Application } from "./application";
 
 export const sketch = (s) => {
+    const application = new Application(400, 400);
 
     // The initial setup function.
     s.setup = () => {
         s.createCanvas(400, 400);
+        s.noStroke();
     };
 
     // The update function.
     s.draw = () => {
-        s.background(0);
+        s.background(255);
+        application.update();
+
+        const grid = application.current_grid;
+
+        for (let y = 0; y < grid.length; y++) {
+            for (let x = 0; x < grid[0].length; x++) {
+                // If the current position in the grid is null, then it is air and we skip it.
+                if (grid[y][x] == null) continue;
+                // Set the fill color to be that of the current particle.
+                s.fill(grid[y][x].color);
+                // We invert the y value by subtracting it from height, because p5 defaults to the top
+                // left of the canvas being the origin point, while we want it to be the bottom left.
+                s.rect(x, s.height - y, 1, 1);
+            }
+        }
     };
 };
 
