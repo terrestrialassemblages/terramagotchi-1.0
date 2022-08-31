86ad50c127c2242cafd10340966cb05c9136b195
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2022-08-31 20:20:07 +1200

    implemented cell width and height to allow for different widths and heights between the app and the canvas resolution

--- src/main.js
+++ src/main.js
@@ -3,17 +3,21 @@ import p5 from "p5";
 import { Application } from "./application";
 
 export const sketch = (s) => {
-    const application = new Application(400, 400);
+    const application = new Application(50, 50);
+    let cell_height, cell_width
 
     // The initial setup function.
     s.setup = () => {
         s.createCanvas(400, 400);
         s.noStroke();
+        application.testInitialiseParticles();
+        cell_height = s.height / application.height
+        cell_width = s.width / application.width
     };
 
     // The update function.
     s.draw = () => {
-        s.background(255);
+        s.background("#87CEEB");
         application.update();
 
         const grid = application.current_grid;
@@ -26,7 +30,12 @@ export const sketch = (s) => {
                 s.fill(grid[y][x].color);
                 // We invert the y value by subtracting it from height, because p5 defaults to the top
                 // left of the canvas being the origin point, while we want it to be the bottom left.
-                s.rect(x, s.height - y, 1, 1);
+                s.rect(
+                    s.width / application.width * x, 
+                    s.height / application.height * (application.height - 1 - y), 
+                    cell_width, 
+                    cell_height
+                );
             }
         }
     };
