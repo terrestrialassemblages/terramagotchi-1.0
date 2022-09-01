18849e7f152f36b4fbb8668d2eb0ae7dcd86a8e7
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2022-09-01 15:40:05 +1200

    initiated terramagotchi-remote package

---
+++ terramagotchi-remote/README.md
@@ -0,0 +1,16 @@
+# Terramagotchi
+
+## Project setup
+```
+npm install
+```
+
+## Build
+```
+npm run build
+```
+
+## Run Development Server
+```
+npm run dev
+```
---
+++ terramagotchi-remote/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/main.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: 'src/index.html'
+    })],
+    devServer: {
+        static: {
+          directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9001,
+    },
+    mode: "development",
+};
