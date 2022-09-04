b343c149381bb876ccf35ce207a66130d88fb4f7
lolgio <ggle406@aucklanduni.ac.nz>
2022-09-04 23:00:52 +1200

    Added darkening on water dispersion

--- terramagotchi/src/particles/organic.js
+++ terramagotchi/src/particles/organic.js
@@ -6,4 +6,19 @@ export class OrganicParticle extends BaseParticle {
         this.nutrient_level = 0;
         this.water_level = 20;
     }
+
+    get_color(s) {
+        // If the colour needs updating and is initialised, decrease brightness by water_level
+        if (this.update_color && this.color !== "#000000") {
+            this.color = s.color(
+                s.hue(this.color),
+                s.saturation(this.color),
+                s.brightness(this.base_color) * this.brightness_offset - this.water_level / 6
+            )
+            this.update_color = false;
+        } else {
+            super.get_color(s);
+        }
+        return this.color;
+    }
 }
