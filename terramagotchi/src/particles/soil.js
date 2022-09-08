47df9089caae5cbafb6cd82bf7c53c8709a23f9f
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2022-09-08 12:17:12 +1200

    Revert "Water transfer in soil (beta) v0.1.0"
    This reverts commit 2e09ed4efed4b9e4baa8e0d0bda3c66fb8c3cfc9.

--- terramagotchi/src/particles/soil.js
+++ terramagotchi/src/particles/soil.js
@@ -7,70 +7,15 @@ export class SoilParticle extends OrganicParticle {
         this.moveable = true;
         this.weight = 2;
         this.water_capacity = 80;
-
-        this.flip = true // testing code
     }
 
     update(x, y, grid) {
-        if (this.flip) {
-            this.flip = false
-            this.water_level = Math.floor((x/240)*170+10) // Code for testing bug pathing
+        if (this.water_level != x) {
+            this.water_level = x // Code for testing bug pathing
             this.update_color = true
             grid.queue_push(x, y)
-            return;
         }
-
-        this.check_transfer_water(x, y, grid)
-        this.check_transfer_nutrients(x, y, grid)
-
-        // Gravity & Erosion come after as movement does not update x/y vars
-        // Could apply nutrient functions to non-soil otherwise
         this.compute_gravity(x, y, grid)
         this.compute_erosion(x, y, grid)
-    }
-
-    check_transfer_water(x, y, grid) {
-
-        /// implementation that lags
-        // let neighbour_offset_arr = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-        // let current_grestest_water = -999
-        // let current_greatest_offset = [0, 0]
-
-        // neighbour_offset_arr.forEach(([ox, oy]) => {
-        //     let current_neighbour = grid.get(x+ox, y+oy)
-        //     if (current_neighbour instanceof SoilParticle && current_neighbour.water_level > current_grestest_water) {
-        //         current_grestest_water = current_neighbour.water_level
-        //         current_greatest_offset = [ox, oy]
-        //     }
-        // })
-
-        // if (current_grestest_water != -999 && current_grestest_water > this.water_level) {
-        //     let neighbour_to_absorb = grid.get(x+current_greatest_offset[0], y+current_greatest_offset[1])
-        //     this.water_level += 10;
-        //     neighbour_to_absorb.water_level -= 10;
-        //     grid.queue_push(x, y)
-        //     grid.queue_push(x+current_greatest_offset[0], y+current_greatest_offset[1])
-        //     this.update_color = true
-        //     neighbour_to_absorb.update_color = true
-        // }
-
-        let neighbour_offset_arr = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-        neighbour_offset_arr.forEach(([ox, oy]) => {
-            let current_neighbour = grid.get(x+ox, y+oy)
-            if (current_neighbour instanceof SoilParticle && current_neighbour.water_level > this.water_level) {
-                this.water_level++;
-                this.update_color = true;
-                current_neighbour.water_level--;
-                current_neighbour.update_color = true;
-                grid.queue_push(x+ox, y+oy)
-            }
-        })
-        if (this.update_color)
-            grid.queue_push(x, y)
-
-    }
-
-    check_transfer_nutrients(x, y, grid) {
-
     }
 }
