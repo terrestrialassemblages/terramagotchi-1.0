c1eab41d67deedb83cb934e20a40ce431fa00841
TaintedEnergy <taintedenergy@gmail.com>
2022-09-09 22:39:37 +1200

    Class list parameter added for water absorption

--- terramagotchi/src/particles/soil.js
+++ terramagotchi/src/particles/soil.js
@@ -6,13 +6,15 @@ export class SoilParticle extends OrganicParticle {
         this.base_color = "#92745B";
         this.moveable = true;
         this.weight = 2;
+
+        this.__water_level = 30;
     }
 
     update(environment) {
         this.compute_gravity(environment)
         this.compute_erosion(environment)
 
-        this.absorb_water(environment, [[0, 1], [0, 1], [1, 0], [0, -1], [-1, 0]]);
-        //this.absorb_water(environment, [[0, 1], [1, 0], [0, -1], [-1, 0]]);
+        this.absorb_water(environment, [[0, 1], [0, 1], [1, 0], [0, -1], [-1, 0]], [SoilParticle]);
+        //this.absorb_water(environment, [[0, 1], [1, 0], [0, -1], [-1, 0]], [SoilParticle]);
     }
 }
