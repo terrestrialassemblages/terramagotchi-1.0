ced997e42273f2a0c4fd59bc1ef8c77e2549adab
dionjw <dionwharerau@icloud.com>
2022-09-14 16:42:45 +1200

    Plant node progress

--- terramagotchi/src/plants/plant_node.js
+++ terramagotchi/src/plants/plant_node.js
@@ -13,7 +13,13 @@ export class PlantNodeParticle extends PlantConstructor {
             this.is_flower = true
         } else {
             this.desired_actions = this.dna.branching_factor.shift()
-            this.desired_actions.sort(function() { return 0.5 - Math.random();})
+        }
+
+        let normalised_current_angle = ((((this.dna.__current_angle + 22.5) / 45 >> 0) % 8) + 8) % 8
+        let normalised_directions =  [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]
+        for (let i = 0; i < this.desired_actions.length; i++) {
+            let norm_i = (((normalised_current_angle + this.desired_actions[i]) % 8) + 8) % 8
+            this.desired_actions = normalised_directions[norm_i]
         }
 
         this.base_color = "#FF0000";
@@ -22,33 +28,40 @@ export class PlantNodeParticle extends PlantConstructor {
     }
 
     update(environment) {
-        if (!this.is_active)
-            return;
+
+        this.absorb_nutrients(environment, this.__living_plant_particle_types)
+        this.absorb_water(environment, this.__living_plant_particle_types)
         
         if (this.is_flower)
             return this.bloom_flower(environment)
-        
-        this.absorb_nutrients(environment, this.__living_plant_particle_types)
-        this.absorb_water(environment, this.__living_plant_particle_types)
 
-        if (this.desired_actions.length == 0) {
+        if (!this.is_active || this.desired_actions.length == 0) {
             this.is_active = false
             return;
         }
 
         if (this.water_level >= this.activation_level && this.nutrient_level >= this.activation_level) {
-            this.perform_action(environment, this.desired_actions.pop())
+            this.desired_actions.sort(function() { return 0.5 - Math.random();})
+            let [action_x, action_y] = this.desired_actions[0]
+
+            if (environment.get(this.x + action_x, this.y + action_y) instanceof AirParticle) {
+                
+                this.perform_first_action(environment)
+
+            }
+
         }
 
     }
 
-    perform_action(environment, direction) {
+    perform_first_action(environment, direction) {
         // Normalise the dna current_angle to each of 8 directions from pixel
         // Start -1 to left and +1 to right, 0 at normalised target pixel
         // Yeet
     }
 
     bloom_flower(environment) {
+        this.is_active = false
         let replacement_particle = new AirParticle(this.x, this.y)
         environment.set(replacement_particle)
     }
