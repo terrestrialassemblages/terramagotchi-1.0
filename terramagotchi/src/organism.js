9d2518ca1edd0a254416ceadd7557ad86c2db85b
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2022-10-01 18:55:13 +1300

    began re-implementation of organism

--- terramagotchi/src/organism.js
+++ terramagotchi/src/organism.js
@@ -1,92 +1,36 @@
 import { CompostParticle } from "./particles";
 
-const MAX_WATER = 300;
-const MAX_FOOD = 300;
-const MAX_LENGTH = 300;
+export class Organism {
+    alive = true;
+    nutrient_capacity = 300;
+    water_capacity = 300;
+    nutrient_level = 50;
+    water_level = 50;
 
+    x = 120;
+    y = 120;
+    facing;
 
-export class Bug {
-    /**
-     * Bug Class represents the organisms inhabiting the terrarium ecosystem.
-     * Responsible for transporting and dispersing nutrients across the environment.
-     * 
-     * @param {int} x   Initial x-coordinate of bug
-     * @param {int} y   Initial y-coordinate of bug
-     */
-    constructor(x = 120, y = 120) {
-        this.x = x
-        this.y = y
-        this.head_color = "#000000"
-        this.body_color = "#FF32FF"
+    color = "#FFF1BE"
 
-        this.water_level = 20
-        this.food_level = 200
-        this.body_length = 1
-        
-        /** energy
-        * arbitrarily from 0 to 100, dictates how often the bug will perform actions
-        * more energy = higher frequency of actions */
-        // this.energy = 10 // just an idea
-
-        this.alive = true
-
-        this.confidence = 10
-        this.state = []
+    constructor() {
+        this.facing = Math.random() < 0.5 ? "EAST" : "WEST";
     }
 
-    update(environment) {
-        let particle_below = environment.get(this.x, this.y-1)
-        let particle_current = environment.get(this.x, this.y)
+    update() {
+        // Check if the organism must die.
+        this.alive = this.nutrient_level > 0 && this.water_level > 0;
 
-        // If the particle below is moveable and has a weight < 1 (e.g air, water, steam)
-        // Then the bug performs the action of falling this frame
-        if (particle_below.moveable && particle_below.weight < 1) {
-            this.move(environment, 0, -1, false)
-            return
-        }
-
-        // If the bug's position overlaps with a heavy particle, move it upwards
-        else if (particle_current.weight >= 1) {
-            if (this.y >= environment.height - 2)
-                this.die(environment)
-            else
-                this.move(environment, 0, 1, false)
-            return;
-        }
-
-        // Now we handle the BUG ACTIONS; base on what it wants/doesn't want
-        let action_probability = Math.min((this.water_level/MAX_WATER), (this.food_level/MAX_FOOD))
-
+        if (this.alive)
+            // Choose a random action and execute it
+            [this.move, this.consume, this.defecate][
+                (Math.random() * 3) >> 0
+            ]();
     }
 
-    move(environment, dx, dy, consume_energy = true) {
-        /** Move function used to handle any action where the bug decides to
-         * move itself to a new location
-         * @param {Environment} environment     The environment which the bug lives on
-         * @param {int} dx                      x-displacement
-         * @param {int} dy                      y-displacement
-         * @param {boolean} consume_energy      Boolean for whether this movement was
-         *                                      done by the bug itself using its own energy
-         *                                      Or due to the environment (e.g., falling)
-         */
+    move() {}
 
-        if (consume_energy) {
-            this.water_level -= 1
-            this.food_level -= 1
-        }
-        
-        environment.get(this.x, this.y).rerender = true;
-        this.x += dx
-        this.y += dy
-    }
+    consume() {}
 
-    die(environment, replace_with_compost = true) {
-        // Function for when conditions are met such that the bug can no longer live
-        // e.g., lack of food/water, lifespan reached, temperature too high, etc.
-
-
-        this.alive = false
-        if (replace_with_compost)
-        environment.set(new CompostParticle(this.x, this.y)) // to be updated with CompostParticle
-    }
+    defecate() {}
 }
