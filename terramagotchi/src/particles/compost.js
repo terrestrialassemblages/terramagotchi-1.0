cbf39d169d3ec3b3e6433e80745ca48f07f40092
TaintedEnergy <taintedenergy@gmail.com>
2022-10-02 21:35:51 +1300

    Separate Grass class, grows upwards

--- terramagotchi/src/particles/compost.js
+++ terramagotchi/src/particles/compost.js
@@ -4,7 +4,7 @@ import { AirParticle } from "./air";
 export class CompostParticle extends OrganicParticle {
     constructor(x, y) {
         super(x, y);
-        this.base_color = "#00FF00";
+        this.base_color = "#664E00";
         this.moveable = true;
         this.weight = 2;
 
--- terramagotchi/src/particles/soil.js
+++ terramagotchi/src/particles/soil.js
@@ -1,26 +1,20 @@
 import { OrganicParticle } from "./organic";
-import { CompostParticle } from "./compost";
 import { AirParticle } from "./air";
+import { CompostParticle } from "./compost";
 
 export class SoilParticle extends OrganicParticle {
     constructor(x, y) {
         super(x, y);
 
-        this.dirt_color = "#92745B";
-        this.grass_color = "#44b300";
-
-        this.base_color = this.dirt_color;
+        this.base_color = "#92745B";
         this.moveable = true;
         this.weight = 2;
 
         this.water_level = 30;
         this.nutrient_level = 30;
 
-        // Chance for this Soil particle to grow as grass underneath grass
-        this.undergrow_chance = 0.3;
-        this.do_undergrowth = Math.random() < this.undergrow_chance;
-        // Poisson distribution chance for color change
-        this.color_change_chance = 0.0005;
+        // Poisson distribution chance to grow grass
+        this.grass_grow_chance = 0.0005;
     }
 
     update(environment) {
@@ -28,42 +22,19 @@ export class SoilParticle extends OrganicParticle {
         this.compute_erosion(environment)
 
         this.absorb_water(environment, [[0, 1], [1, 0], [0, -1], [-1, 0]], [SoilParticle]);
-
         this.absorb_nutrients(environment, [[0, 1], [1, 0], [0, -1], [-1, 0]], [SoilParticle]);
 
-        this.compute_grass(environment);
+        this.grass_growth(environment);
     }
 
-    compute_grass(environment) {
-        // Has not moved
-        if (this.moveable_x && this.moveable_y) {
-            let particle_above = environment.get(this.x,this.y+1);
+    grass_growth(environment) {
 
-            // Air is above soil or grass is above soil and soil can do undergrowth
-            if (particle_above instanceof AirParticle || 
-                (this.do_undergrowth && particle_above instanceof SoilParticle && particle_above.is_grass)) {
-                // Soil is not green (grass color)
-                if (this.base_color != this.grass_color) {
-                    // Gradually turn soil color to green via Poisson Distribution
-                    if (Math.random() < this.color_change_chance) {
-                        this.base_color = this.grass_color;
-                        this.color = "#000000";
-                        this.rerender = true;
-                    }
-                }
-            }
-            // Air is not above soil and no undergrowth
-            else {
-                // Soil is not brown (dirt color)
-                if (this.base_color != this.dirt_color) {
-                    // Gradually turn soil color to brown via Poisson Distribution
-                    if (Math.random() < this.color_change_chance) {
-                        this.base_color = this.dirt_color;
-                        this.color = "#000000";
-                        this.rerender = true;
-                    }
-                }
-            }
+        // Has not moved, Particle above is Air and Poisson Distribution chance
+        if (this.moveable_x && this.moveable_y && 
+            environment.get(this.x,this.y+1) instanceof AirParticle && 
+            Math.random() < this.grass_grow_chance) {
+            // Grow grass
+            environment.set(new GrassParticle(this.x,this.y+1));
         }
     }
 
@@ -85,5 +56,47 @@ export class SoilParticle extends OrganicParticle {
                 this.water_level / 4
         );
         return this.color;
+    }
+}
+
+class GrassParticle extends SoilParticle {
+    constructor(x, y) {
+        super(x, y);
+        this.base_color = "#44B300";
+
+        this.water_level = 0;
+        this.nutrient_level = 0;
+
+        // Chance for this Grass particle to grow more grass above it
+        this.grow_chance = 0.2;
+        this.grow_grass = Math.random() < this.grow_chance;
+
+        // Poisson distribution chance to die
+        this.grass_death_chance = 0.001;
+    }
+
+    update(environment) {
+        this.compute_gravity(environment);
+
+        this.absorb_water(environment, [[0, 1], [1, 0], [0, -1], [-1, 0]], [SoilParticle]);
+        this.absorb_nutrients(environment, [[0, 1], [1, 0], [0, -1], [-1, 0]], [SoilParticle]);
+
+        if (this.grow_grass) this.grass_growth(environment);
+        this.grass_death(environment);
+    }
+
+    grass_death(environment) {
+
+        let particel_above = environment.get(this.x,this.y+1);
+
+        // Particle above is not Air or Grass, and Poisson Distribution chance
+        if (!(particel_above instanceof AirParticle || particel_above instanceof GrassParticle) && 
+            Math.random() < this.grass_death_chance) {
+            // Kill Grass (Turn into Compost)
+            let new_compost = new CompostParticle(this.x,this.y);
+            new_compost.nutrient_content = this.nutrient_level;
+            new_compost.water_content = this.water_level;
+            environment.set(new_compost);
+        }
     }
 }
