05edef1e448f810a3fe3b6c3f2a6c06073764a68
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2022-10-03 00:17:56 +1300

    organism now walks toward a target location.

--- terramagotchi/src/organism.js
+++ terramagotchi/src/organism.js
@@ -10,8 +10,13 @@ export class Organism {
 
     x = 120;
     y = 120;
+    target_location = [120, 120];
 
     color = "#FFF1BE";
+
+    constructor() {
+        this.seek();
+    }
 
     update(environment) {
         this.compute_gravity(environment);
@@ -19,9 +24,16 @@ export class Organism {
         // Check if the organism must die.
         this.alive = this.nutrient_level > 0 && this.water_level > 0; // NOTE: DOES THIS NEED TO BE A CLASS VAR??
 
-        if (this.alive && Math.random() < 0.1)
+        if (this.alive) {
             // NOTE: CONSIDER USING A TICK COUNTER INSTEAD OF RANDOM
-            this.move(environment);
+            if (Math.random() < 0.1) this.move(environment);
+
+            if (
+                this.x == this.target_location[0] &&
+                this.y == this.target_location[1]
+            )
+                this.seek(environment);
+        }
     }
 
     compute_gravity(environment) {
@@ -32,13 +44,34 @@ export class Organism {
         }
     }
 
+    seek(environment) {
+        /*
+            Seek for the next target location.
+        */
+        this.target_location = [
+            (this.x + Math.random() * 100 - 50) >> 0,
+            (this.y + Math.random() * 100 - 50) >> 0,
+        ];
+    }
+
     move(environment) {
         /*
-            Performs a random walk.
-            TODO: Implement pathfinding towards water / deadplants.
+            Move towards the target location.
         */
 
-        const valid_neighbours = [];
+        const valid_neighbours = this.__get_valid_neighbours(environment);
+
+        if (valid_neighbours.length > 0) {
+            environment.get(this.x, this.y).rerender = true;
+            const [new_x, new_y] =
+                this.__choose_best_neighbour(valid_neighbours);
+            this.x = new_x;
+            this.y = new_y;
+        }
+    }
+
+    __get_valid_neighbours(environment) {
+        let valid_neighbours = [];
         for (let [neighbour_x, neighbour_y] of [
             [0, 1],
             [1, 0],
@@ -68,20 +101,36 @@ export class Organism {
                     secondary_neighbour_particle.weight > 1 &&
                     !(secondary_neighbour_particle instanceof BoundaryParticle)
                 ) {
-                    valid_neighbours.push([neighbour_x, neighbour_y]);
+                    valid_neighbours.push([
+                        this.x + neighbour_x,
+                        this.y + neighbour_y,
+                    ]);
                     break;
                 }
             }
         }
+        return valid_neighbours;
+    }
 
-        if (valid_neighbours.length > 0) {
-            const [move_x, move_y] =
-                valid_neighbours[
-                    (Math.random() * valid_neighbours.length) >> 0
-                ];
-            this.x += move_x;
-            this.y += move_y;
+    __choose_best_neighbour(valid_neighbours) {
+        let best_neighbour = valid_neighbours[0];
+        let best_distance =
+            Math.abs(this.target_location[0] - best_neighbour[0]) +
+            Math.abs(this.target_location[1] - best_neighbour[1]); // Manhattan distance
+        for (let neighbour of valid_neighbours) {
+            let distance =
+                Math.abs(this.target_location[0] - neighbour[0]) +
+                Math.abs(this.target_location[1] - neighbour[1]);
+            if (
+                distance < best_distance ||
+                (distance == best_distance &&
+                    Math.random() < 1 / valid_neighbours.length)
+            ) {
+                best_neighbour = neighbour;
+                best_distance = distance;
+            }
         }
+        return best_neighbour;
     }
 
     consume(environment) {}
