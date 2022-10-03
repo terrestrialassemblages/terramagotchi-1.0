1ce9bd9afacf0241bf17623f739c6d3e3d710b75
TaintedEnergy <taintedenergy@gmail.com>
2022-10-03 17:02:27 +1300

    10 minute day-night cycle

--- terramagotchi/src/main.js
+++ terramagotchi/src/main.js
@@ -4,6 +4,7 @@ import { Application } from "./application";
 
 // Testing code: Imports for testing particles by manually adding
 import {
+    AirParticle,
     SoilParticle,
     StoneParticle,
     WaterParticle,
@@ -21,7 +22,12 @@ export const sketch = (s) => {
     const application = new Application(240, 240);
     let cell_size = 3; // Defines cell size in pixels.
 
-    let main, overlay;
+    let night_overlay, main, sky;
+
+    let sky_day_color = s.color(135,206,235);
+    let sky_night_color = s.color(0, 11, 31);
+
+    let night_overlay_opacity = 150;
 
     // The initial setup function.
     s.setup = () => {
@@ -34,9 +40,8 @@ export const sketch = (s) => {
         main = s.createGraphics(s.width, s.height);
         main.noStroke();
 
-        overlay = s.createGraphics(s.width, s.height);
-        overlay.clear();
-        overlay.background(0, 0, 0, 50);
+        night_overlay = s.createGraphics(s.width, s.height);
+        sky = s.createGraphics(s.width, s.height);
 
         s.colorMode(s.HSB);
         // s.frameRate(20);
@@ -51,19 +56,35 @@ export const sketch = (s) => {
         // have changed and need to be rendered.
         for (let particle of application.environment.particle_grid) {
             if (particle.rerender) {
-                particle.rerender = false;
-                main.fill(particle.get_color(s));
+                if (!(particle instanceof AirParticle)) {
+                    main.noErase()
+                    main.fill(particle.get_color(s));
+                }
+                else {
+                    main.erase()
+                }
+
                 main.rect(
                     cell_size * particle.x,
                     cell_size * (application.height - 1 - particle.y),
                     cell_size,
                     cell_size
                 );
+                particle.rerender = false;
             }
         }
+
+        // Render background sky
+        s.image(sky, 0, 0);
+        s.background(s.lerpColor(sky_night_color, sky_day_color, application.environment.light_level / 100));
+
+        // Render main environment grid
         s.image(main, 0, 0);
 
-        s.image(overlay, 0, 0);
+        // Render night-time darkening overlay
+        night_overlay.clear();
+        night_overlay.background(0, 0, 0, s.lerp(night_overlay_opacity, 0, application.environment.light_level / 100));
+        s.image(night_overlay, 0, 0);
     };
 
     // Debug code for drawing
