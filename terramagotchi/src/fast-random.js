701d2830d5f884917541eab87d16cbd8f3c4fa68
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2022-10-09 23:57:32 +1300

    increase N, add documentation comments

--- terramagotchi/src/fast-random.js
+++ terramagotchi/src/fast-random.js
@@ -1,16 +1,18 @@
-const N = 1000000
+const N = 10000000
 
 export class FastRandom {
     static numbers = []
     static pointer = -1
 
     static random() {
+        // Returns a random floating point value in the range [0, 1)
         this.pointer++
         if (this.pointer >= N) this.pointer = 0
         return this.numbers[this.pointer]
     }
 
     static random_int(max) {
+        // Returns a random integer value in the range [0, max)
         return (this.random() * max) | 0
     }
     
