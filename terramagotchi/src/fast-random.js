41c59e3b73ceaf363944c2d36b7298a88163e9a0
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2022-10-10 17:24:58 +1300

    add FastRandom.choice() and .int_min_max(), change random_int() to int_max()

--- terramagotchi/src/fast-random.js
+++ terramagotchi/src/fast-random.js
@@ -5,17 +5,35 @@ export class FastRandom {
     static pointer = -1
 
     static random() {
-        // Returns a random floating point value in the range [0, 1)
+        /**
+         * Returns a random floating point value in the range [0, 1)
+         */
         this.pointer++
         if (this.pointer >= N) this.pointer = 0
         return this.numbers[this.pointer]
     }
 
-    static random_int(max) {
-        // Returns a random integer value in the range [0, max)
-        return (this.random() * max) | 0
+    static int_max(max) {
+        /**
+         * Returns a random integer value in the range [0, max] (inclusive)
+         */
+        return (this.random() * (max + 1)) | 0
     }
-    
+
+    static int_min_max(min, max) {
+        /**
+         * Returns a random integer value in the range [min, max] (inclusive)
+         */
+        return (min + this.random() * (max + 1 - min)) | 0
+    }
+
+    static choice(list) {
+        /**
+         * Returns a random item from the list.
+         */
+        return list[(this.random() * list.length) | 0]
+    }
+
     static {
         for (let i = 0; i < N; i++) {
             this.numbers.push(Math.random())
