490053781b769c140cdc6f9f1d654b6129b87d75
lolgio <ggle406@aucklanduni.ac.nz>
2022-10-16 12:00:11 +1300

    User cooldown variable in firestore

--- terramagotchi-firebase/functions/index.js
+++ terramagotchi-firebase/functions/index.js
@@ -2,7 +2,7 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp(functions.config().firebase);
 
-const COOLDOWN_LENGTH = process.env.COOLDOWN_LENGTH;
+const DEFAULT_COOLDOWN_LENGTH = 1000;
 
 // Singular cloud function for user interaction, parses document and instance_id as the parameter
 exports.userInteract = functions
@@ -13,6 +13,11 @@ exports.userInteract = functions
     .region("australia-southeast1")
     .https.onCall((data, context) => {
         const colRef = admin.firestore().collection(data.instance_id);
+
+        // "!global" collection is used to store variables, it cannot be used as a instance id
+        if (data.instance_id === "!global") {
+            return { message: `Cannot use global as an instance!` }
+        }
 
         // Create a new instance if given "init" as the document param
         if (data.document === "init") {
@@ -25,30 +30,35 @@ exports.userInteract = functions
             return { message: `Successfully created instance ${data.instance_id}` }
         }
 
-        const docRef = colRef.doc(data.document);
-        const timestamp = Date.now();
-        const uid = context.auth.uid;
+        return admin.firestore().collection("!global").doc("variables").get().then((global) => {
+            const docRef = colRef.doc(data.document);
+            const timestamp = Date.now();
+            const uid = context.auth.uid;
 
-        return colRef.doc(uid).get().then((user) => {
-            let time_passed = user.exists ? timestamp - user.data().last_interaction : COOLDOWN_LENGTH;
-            // Check if it has been 5 seconds since the user's last interaction
-            if (time_passed < COOLDOWN_LENGTH) {
-                return { message: COOLDOWN_LENGTH - time_passed }
-            } else {
-                // Update the specified variable document
-                return docRef.get().then((doc) => {
-                    if (doc.exists) {
-                        docRef.update({
-                            value: doc.data().value + 1,
-                        });
-                        // Set timestamp for user
-                        colRef.doc(uid).set({ last_interaction: timestamp });
-                        return { message: `Successfully sent ${data.document}` }
-                    }
-                    return { message: `Unknown instance: ${data.instance_id}` }
-                });
-            }
-        }).catch((reason) => {
-            return { message: reason.message }
+            // If the global variables document doesn't exist, use default user_cooldown
+            const cooldown_length = global.exists ? global.data().user_cooldown : DEFAULT_COOLDOWN_LENGTH;
+
+            return colRef.doc(uid).get().then((user) => {
+                let time_passed = user.exists ? timestamp - user.data().last_interaction : cooldown_length;
+                // Check if it has been 5 seconds since the user's last interaction
+                if (time_passed < cooldown_length) {
+                    return { message: cooldown_length - time_passed }
+                } else {
+                    // Update the specified variable document
+                    return docRef.get().then((doc) => {
+                        if (doc.exists) {
+                            docRef.update({
+                                value: doc.data().value + 1,
+                            });
+                            // Set timestamp for user
+                            colRef.doc(uid).set({ last_interaction: timestamp });
+                            return { message: `Successfully sent ${data.document}` }
+                        }
+                        return { message: `Unknown instance: ${data.instance_id}` }
+                    });
+                }
+            }).catch((reason) => {
+                return { message: reason.message }
+            });
         });
     });
