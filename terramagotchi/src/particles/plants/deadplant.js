fec9f55233caa3caa0d3d1567c5a5bd5282931fe
TaintedEnergy <taintedenergy@gmail.com>
2022-10-23 17:12:37 +1300

    Deadplant compacting

--- terramagotchi/src/particles/plants/deadplant.js
+++ terramagotchi/src/particles/plants/deadplant.js
@@ -3,8 +3,9 @@ import { OrganicParticle } from "../organic";
 import { StemParticle, BarkParticle, LeafParticle } from ".";
 import { FastRandom } from "../../fast-random";
 import { AirParticle } from "../air";
+import { Environment } from "../../environment";
 
-const DESPAWN_IF_EMPTY_CHANCE = 0.1
+const DESPAWN_IF_EMPTY_CHANCE = 0.01
 
 export class DeadPlantParticle extends OrganicParticle {
     constructor(x, y, plant_dna=null) {
@@ -26,6 +27,8 @@ export class DeadPlantParticle extends OrganicParticle {
         if (this.can_erode) this.compute_erosion(environment)
         this.compute_gravity(environment)
 
+        this.compute_compact(environment)
+
         if (
             this.water_level == 0 &&
             this.nutrient_level == 0 &&
@@ -35,4 +38,55 @@ export class DeadPlantParticle extends OrganicParticle {
         }
     }
 
+    /**
+     * Disperses water and nutrient levels from this particle to-
+     * a lower DeadPlantParticle particle
+     * 
+     * @param {Environment} environment 
+     */
+    compute_compact(environment) {
+        let particle_below = environment.get(this.x, this.y - 1);
+
+        // Particle below is also a DeadPlantParticle
+        if (particle_below instanceof DeadPlantParticle) {
+            
+            // Attempt nutrient dispersion
+            if (particle_below.nutrient_level < particle_below.nutrient_capacity) {
+                // Transfer as much nutrient as possible to particle_below
+                let transfer_amount = Math.min(
+                    this.nutrient_level,
+                    particle_below.nutrient_capacity -
+                        particle_below.nutrient_level
+                );
+                particle_below.nutrient_level += transfer_amount;
+                this.nutrient_level -= transfer_amount;
+            }
+
+            // Attempt water dispersion
+            if (particle_below.water_level < particle_below.water_capacity) {
+                // Transfer as much water as possible to particle_below
+                let transfer_amount = Math.min(
+                    this.water_level,
+                    particle_below.water_capacity -
+                        particle_below.water_level
+                );
+                particle_below.water_level += transfer_amount;
+                this.water_level -= transfer_amount;
+            }
+        }
+    }
+
+    get_color(s) {
+        // Initialise colour if needed
+        if (this.color === "#FF00FF") {
+            super.get_color(s);
+        }
+
+        this.color = s.color(
+            s.hue(this.color),
+            s.saturation(this.base_color) * this.saturation_offset,
+            s.brightness(this.base_color) * this.brightness_offset
+        );
+        return this.color;
+    }
 }
