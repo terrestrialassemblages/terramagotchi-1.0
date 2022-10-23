e30aefcaef80d98a1b8793bf725562eb21cb7379
RadishRadbino <blakevincentlouis@gmail.com>
2022-10-23 16:20:41 +1300

    added custom sunflower roots

--- terramagotchi/src/particles/plants/plant_generator.js
+++ terramagotchi/src/particles/plants/plant_generator.js
@@ -550,6 +550,10 @@ switch (TREE_TYPE) {
         tree_direction = [-1, 1][FastRandom.int_max(1)]
         tree_angle_offset = -14
         return {
+            root_node_spawn_distance: 6,
+            root_length_max: 5,
+            root_max_curve_length: 2,
+            root_minimum_distance: 1,
             seed_activation_level: 0,
             node_activation_level: 1,
             color: "#3F7B25", // color inspo: https://colorswall.com/palette/34441
